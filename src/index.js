6881c120d8292a5b430a7d527f6bf2658d34d0be
apli.ai <akumbarwar18@gmail.com>
2022-01-13 21:31:50 +0530

    Photowall Initial

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom'; 
 import Main from './Components/Main';
-
+import './styles/stylesheet.css'
 
 const tasks = ['Do something', 'when should I', 'Ohh no gone'];
 
